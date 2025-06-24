99b8749c26424fcfca4cea1155bd4a20544e38c5
Fatma <laidanifatma@gmail.com>
2025-06-24 07:26:26 +0100

    add apps accounts,appointments,patients,dashborad

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'doctors',
+    'accounts',
+    'appointments',
+    'patients',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
