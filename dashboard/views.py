36e05bc00d713818431d2b06b93066e14bcd8c28
Fatma <laidanifatma@gmail.com>
2025-06-25 11:35:17 +0100

    Add folder static in project

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def home(request):
+    return render(request,'dashboard/home.html')
+
+
 def dashboard(request):
     return HttpResponse("Hello dashboard")
 
--- myproject/urls.py
+++ myproject/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,6 @@ urlpatterns = [
     path('',include('patients.urls')),
     path('',include('dashboard.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / "static")
